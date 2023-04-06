c1a5f104009c12aeebc3057babe2bbf5b42c80dd
flynth <flynth.dev@gmail.com>
2023-04-05 22:55:36 -0400

    Change background color 0,0.05,0.1,1 ;

--- basic/1.syntax/source/main.cpp
+++ basic/1.syntax/source/main.cpp
@@ -6,8 +6,8 @@ int main() {
   //colors
   colorWin color;
   color.red=0.0f;
-  color.green=0.4;
-  color.blue=0.4f;
+  color.green=0.05;
+  color.blue=0.1f;
   color.alpha=1;
   app.Init(1000,1000,"ProWin",color);
 
