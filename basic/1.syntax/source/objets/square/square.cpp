f068e9ca2b44dbf3a7ccf8000272fa11c374f910
flynth <flynth.dev@gmail.com>
2023-04-06 21:24:25 -0400

    Change from 5 vertices to 8 in square class

--- basic/1.syntax/source/objets/square/square.cpp
+++ basic/1.syntax/source/objets/square/square.cpp
@@ -2,23 +2,17 @@
 #include <iostream>
 using namespace std;
 
-
-//class trian
-//
-
 void Square::addShader(const char *vertexShader, const char *fragShader){
   shader=new Shader();
   shader->load(vertexShader, fragShader );
 }
 void Square::addBuffer(float *vertices, unsigned int sizeVert, unsigned int *indices, unsigned int sizeInd){
 
-  // buffer
     glGenVertexArrays(1, &VAO);
     glGenBuffers(1, &VBO);
     glGenBuffers(1, &EBO);
 
     glBindVertexArray(VAO);
-
     glBindBuffer(GL_ARRAY_BUFFER, VBO);
     glBufferData(GL_ARRAY_BUFFER, sizeVert, vertices, GL_STATIC_DRAW);
 
@@ -26,11 +20,15 @@ void Square::addBuffer(float *vertices, unsigned int sizeVert, unsigned int *ind
     glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeInd , indices, GL_STATIC_DRAW);
 
     // position attribute
-    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 5 * sizeof(float), (void*)0);
+    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 8 * sizeof(float), (void*)0);
     glEnableVertexAttribArray(0);
-    // texture coord attribute
-    glVertexAttribPointer(1, 2, GL_FLOAT, GL_FALSE, 5 * sizeof(float), (void*)(3 * sizeof(float)));
+    // color attribute
+    glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, 8 * sizeof(float), (void*)(3 * sizeof(float)));
     glEnableVertexAttribArray(1);
+    // texture coord attribute
+    glVertexAttribPointer(2, 2, GL_FLOAT, GL_FALSE, 8 * sizeof(float), (void*)(6 * sizeof(float)));
+    glEnableVertexAttribArray(2);
+    
 }
 void Square::Init(){} ;
 void Square::Update(){
